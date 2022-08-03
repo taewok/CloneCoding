7aacc3d1d31e9d25ba686f8622c522e2c2c86dd3
taewok <taewok5163@naver.com>
2022-08-03 18:15:55 +0900

    받아온 영화 정보를 스타일링

--- nomardcoder/next.config.js
+++ nomardcoder/next.config.js
@@ -1,7 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const API_KEY = "27c778fbef0dd062df41b4202379a763";
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
--- nomardcoder/pages/index.js
+++ nomardcoder/pages/index.js
@@ -1,14 +1,13 @@
 import { useEffect, useState } from "react";
 import Seo from "./components/Seo";
 
-const API_KEY = "10923b261ba94d897ac6b81148314a3f";
-
 export default function Home() {
   const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
+      const { results } = await (await fetch("https://api.themoviedb.org/3/movie/popular?api_key=27c778fbef0dd062df41b4202379a763")).json();
       setMovies(results);
+      console.log(results)
     })();
   }, []);
   return (
@@ -17,31 +16,31 @@ export default function Home() {
       {!movies && <h4>Loading...</h4>}
       {movies?.map((movie) => (
         <div className="movie" key={movie.id}>
-        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-        <h4>{movie.original_title}</h4>
-      </div>
-    ))}
-    <style jsx>{`
-      .container {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        padding: 20px;
-        gap: 20px;
-      }
-      .movie img {
-        max-width: 100%;
-        border-radius: 12px;
-        transition: transform 0.2s ease-in-out;
-        box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-      }
-      .movie:hover img {
-        transform: scale(1.05) translateY(-10px);
-      }
-      .movie h4 {
-        font-size: 18px;
-        text-align: center;
-      }
-    `}</style>
-  </div>
-);
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  );
 }
