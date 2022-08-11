75b3f885ce0bd3cbda161e16ad746217314ccbe1
taewok <taewok5163@naver.com>
2022-08-11 17:54:52 +0900

    1차 문제 nav에 메뉴 마우스 hover시 밑줄 유지 실패

--- webdesign/pages/index.js
+++ webdesign/pages/index.js
@@ -2,25 +2,433 @@ import React from "react";
 
 const index = () => {
   return (
-    <div>
-      <div className="divbox">dfsdfs</div>
+    <div className="main_box">
+      <header>
+        <div className="head_wrap">
+          <ul>
+            <li>LOGIN</li>
+            <li>JOIN</li>
+            <li>+BOOKMARK</li>
+          </ul>
+          <span className="main_title">MINIPET</span>
+          <form className="search_form">
+            <input placeholder="검색어를 입력하세요."></input>
+            <img src="https://minipetmall.co.kr/pc/images/icon/search.png"></img>
+          </form>
+        </div>
+        <nav>
+          <ul>
+            <li>ABOUT MINIPET</li>
+            <div className="hide_box">
+              <ul>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/11.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/11_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">대표인사말</p>
+                    <p className="txt">미니펫 대표의 신념, 고객을 위한 약속</p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/12.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/12_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">회사소개</p>
+                    <p className="txt">반려동물 분양 만족도 1위 미니펫을 소개합니다. </p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/13.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/13_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">스텝소개</p>
+                    <p className="txt">미니펫과 함께 하는 스텝을 소개합니다. </p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/14.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/14_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">채용정보</p>
+                    <p className="txt">함께 일해 볼래요? 브리더, 스텝 大모집 </p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/15.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/15_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">가맹문의</p>
+                    <p className="txt">눈 앞의 이익만을 쫓는 프랜차이즈 영업은 하지 않습니다.</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <li>분양시스템</li>
+            <div className="hide_box">
+              <ul>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/21.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/21_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">사후관리</p>
+                    <p className="txt">미니펫만의 특별한 평생 서비스</p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/22.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/22_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">분양시스템</p>
+                    <p className="txt">20년 전통의 체계적인 시스템</p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/23.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/23_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">안심배송</p>
+                    <p className="txt">안전최우선, 전국 어디든 달려갑니다</p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/24.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/24_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">제휴업체</p>
+                    <p className="txt">미니펫 가족을 위한 차별화된 혜택</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <li>ALL PET</li>
+            <div className="hide_box">
+              <ul>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/31.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/31_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">전체보기</p>
+                    <p className="txt">미니펫에서 현재 분양중인 아가들입니다.</p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/32.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/32_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">프리미엄</p>
+                    <p className="txt">최상위 등급의 1% 아가들을 만나보세요.</p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/33.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/33_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">견종/묘종별 모아보기</p>
+                    <p className="txt">다양한 견종/묘종별로 아가들을 보실 수 있어요.</p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/34.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/34_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">지점별 모아보기</p>
+                    <p className="txt">나와 가까운 미니펫은 어디일까요?</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <li>MINIPET</li>
+            <div className="hide_box">
+              <ul>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/31.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/31_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">전체보기</p>
+                    <p className="txt">미니펫에서 현재 분양중인 아가들입니다.</p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/32.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/32_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">프리미엄</p>
+                    <p className="txt">최상위 등급의 1% 아가들을 만나보세요.</p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/33.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/33_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">견종별 모아보기</p>
+                    <p className="txt">다양한 견종별로 아가들을 보실 수 있어요.</p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/34.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/34_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">지점별 모아보기</p>
+                    <p className="txt">나와 가까운 미니펫은 어디일까요?</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <li>MINICAT</li>
+            <div className="hide_box">
+              <ul>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/41.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/41_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">전체보기</p>
+                    <p className="txt">미니펫에서 현재 분양중인 아가들입니다.</p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/42.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/42_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">프리미엄</p>
+                    <p className="txt">최상위 등급의 1% 아가들을 만나보세요.</p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/43.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/43_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">묘종별 모아보기</p>
+                    <p className="txt">다양한 묘종별로 아가들을 보실 수 있어요.</p>
+                  </div>
+                </li>
+                <li>
+                  <div className="img_box">
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/44.jpg"/>
+                    <img src="https://minipetmall.co.kr/pc/images/gnb/44_on.jpg"/>
+                  </div>
+                  <div className="txt_box">
+                    <p className="tit">지점별 모아보기</p>
+                    <p className="txt">나와 가까운 미니펫은 어디일까요?</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <li>입양후기</li>
+            <li>반려 지식백과</li>
+            <li>WEDDING</li>
+            <li>OVERSEAS ADOPTION</li>
+            <li>Q&amp;A</li>
+            <li>지점안내</li>
+          </ul>
+        </nav>
+      </header>
       <style jsx>{`
-        .divbox {
-          width: 100px;
-          height: 60px;
-          background-color: red;
+        * {
+          font-family: "Noto Sans KR", sans-serif;
+          list-style: none;
+        }
+
+
+        header {
+          height: 145px;
+          margin: 0 auto;
+        }
+
+
+        .head_wrap {
+          width: 100%;
+          height: 90px;
+          display: flex;
+          justify-content: space-evenly;
+          align-items: center;
+        }
+        .head_wrap > ul {
+          width: 200px;
+          display: flex;
+          padding: 0;
+          font-size: 16px;
+        }
+        .head_wrap > ul > li {
+          margin-left: 20px;
+        }
+        .head_wrap > ul > li:nth-child(1) {
+          margin-left: 0px;
+        }
+        .head_wrap > ul > li:nth-child(3) {
+          color: #c29683;
+          font-weight: bolder;
+        }
+
+
+        .search_form {
           position: relative;
         }
-        .divbox::after {
+        .search_form > input {
+          height: 30px;
+          width: 200px;
+          padding: 0px 30px 0px 10px;
+          border-bottom: 2px solid black;
+          border-top: none;
+          border-left: none;
+          border-right: none;
+          position: relative;
+        }
+        .search_form > input:focus {
+          outline: none;
+        }
+        .search_form > img {
+          width: 18px;
+          height: 18px;
+          top: 8px;
+          position: absolute;
+          right: 5px;
+        }
+
+
+        .main_title {
+          font-size: 40px;
+        }
+
+
+        nav {
+          width: 100%;
+          height: 53px;
+          font-size: 16px;
+          color: #333333;
+          border-top: 1px solid rgb(205, 205, 205);
+          box-sizing: border-box;
+        }
+        nav > ul {
+          width: 1400px;
+          display: flex;
+          margin: 0 auto;
+          justify-content: center;
+          position:relative;
+        }
+        nav > ul > li {
+          height: 53px;
+          line-height: 53px;
+          padding: 0px 23px;
+          position: relative;
+          cursor: pointer;
+        }
+        nav > ul > li::after {
           content: "";
           position: absolute;
-          right: -30px;
-          top: 50%;
-          margin-top: -20px;
-          border-top: 20px solid transparent;
-          border-right: 20px solid transparent;
-          border-bottom: 20px solid transparent;
-          border-left: 20px solid red;
+          width: 0px;
+          height: 3px;
+          margin: 0 auto;
+          background: #c29683;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          transition: 400ms width;
+        }
+        nav > ul > li:hover::after {
+          width: 75%;
+        }
+        nav > ul > li:hover + .hide_box{
+          display:block;
+        }
+
+
+        .hide_box{
+          width:100vw;
+          position:absolute;
+          top:53px;
+          border: 1px solid rgb(205,205,205);
+          display:none;
+        }
+        .hide_box:hover{
+          display:block;
+        }
+        .hide_box>ul{
+          width:1230px;
+          display:flex;
+          margin:0 auto;
+          padding:30px 0px;
+          height:186px;
+        }
+        .hide_box>ul>li{
+          width:25%;
+          display:flex;
+          flex-direction:column;
+          align-items:center;
+          padding:0px 15px;
+          cursor:pointer;
+        }
+        .hide_box>ul>li:hover>.txt_box>.txt{
+          color:rgb(104, 104, 104);;
+        }
+        .hide_box>ul>li:hover>.img_box>img:nth-child(1){
+          opacity:0;
+        }
+        
+
+        .txt_box{
+          text-align:center;
+        }
+        .tit{
+          font-size:18px;
+          color:#111;
+          font-weight:500;
+        }
+        .txt{
+          color:#999;
+        }
+
+        
+        .img_box{
+          position:relative;
+          height:auto;
+          width:100%;
+        }
+        .img_box>img{
+          width:100%;
+        }
+        .img_box>img:nth-child(1){
+          position:absolute;
+          left:0;
         }
       `}</style>
     </div>
